8b107899f663bcef669f4545442dc12c8f748df3
Candace Wilson <candaceyw@gmail.com>
2020-02-13 22:06:25 -0500

    app created and fetching data

---
+++ github-user/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './App.css';
+import UserCard from './components/UserCard'
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      followers: []
+    };
+  }
+
+  fetchUser = () => {
+    fetch("https://api.github.com/users/candaceyw")
+    .then(res => {
+      return res.json();
+    })
+    .then(cards => {
+      this.setState({ user: cards });
+      console.log(cards);
+    })
+    .catch(error => {
+      console.log('No cards to show', error);
+    });
+};
+
+fetchFollower = () => {
+  fetch("https://api.github.com/users/candaceyw/followers")
+  .then(res => {
+    return res.json();
+  })
+  .then(cards => {
+    this.setState({ followers: cards });
+    console.log(cards);
+  })
+  .catch(error => {
+    console.log('No followers to show', error);
+  });
+};
+
+
+componentDidMount() {
+  console.log('mounted')
+  this.fetchUser();
+  this.fetchFollower();
+
+}
+
+  render(){
+    console.log(this.state.user)
+
+  return (
+    <div className="App">
+        GitHub Cards
+    <UserCard 
+    img={this.state.user.avatar_url}
+    name={this.state.user.login}
+    location={this.state.user.location}
+    bio={this.state.user.bio}
+    url={this.state.user.html_url}
+    />
+    </div>
+  );
+}
+}
+export default App;
---
+++ github-user/src/components/FollowersCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class FollowersCard extends Component {
+    render() {
+        return (
+            <div className="followersCard">
+                {/* <img src={this.props.img} alt="avatar"/> */}
+            </div>
+        )
+    }
+}
+
+export default FollowersCard;
