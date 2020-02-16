c6a701e6686816afb02ee98bdadf37701a29c2df
Candace Wilson <candaceyw@gmail.com>
2020-02-16 14:04:09 -0500

    Stretch -github contribution calendar

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './sass/output.css'
 import UserCard from './components/UserCard'
 import Followers from './components/Followers';
+import GitHubCalendar from 'react-github-calendar';
 
 class App extends Component {
   constructor() { 
@@ -55,24 +56,45 @@ componentDidMount() {
 
   return (
     <div className="App">
-        GitHub Cards
+
+        <div className="header">
+
+       
+                  <UserCard 
+              img={this.state.user.avatar_url}
+              name={this.state.user.name}
+              login={this.state.user.login}
+
+              location={this.state.user.location}
+              bio={this.state.user.bio}
+              url={this.state.user.html_url}
+              />
+                
+        <div className="header__text-box">
+            <h1 class="heading-primary"> 
+                <span class="heading-primary--main">GitHub</span>
+                <span class="heading-primary--sub">where dev lives</span>
+            </h1>
+          <div>
+              <h1 className="heading heading-primary"> GitHub Contribution Calendar</h1>
+              <GitHubCalendar username="candaceyw" fontSize={12} blockSize={10} blockMargin={4} />
+          </div>
+        </div>
+        </div>   
+       
         <div className="row">
 <div></div>
-    <UserCard 
-    img={this.state.user.avatar_url}
-    name={this.state.user.name}
-    login={this.state.user.login}
-
-    location={this.state.user.location}
-    bio={this.state.user.bio}
-    url={this.state.user.html_url}
-    />
-    <div></div>
-
+  
+ 
     </div>
 
     <div>
-      <h1>GitHub Followers</h1>
+      
+      <div class="u-center-text u-margin-bottom-big">
+                    <h2 class="heading-secondary">
+                    GitHub Followers
+                    </h2>
+                </div>
     
       <div className="row">
         {this.state.followers.map(user => {
